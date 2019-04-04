98e13587f9b0cab16cceea5da5184ec36bdd8ff5
Carlos Calderon <cdcalderon@gmail.com>
2019-04-04 17:10:27 -0500

    Create app Layout components, Login, register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IncomeExpenseComponent } from './income-expense/income-expense.component';
+import { ChartComponent } from './income-expense/chart/chart.component';
+import { DetailComponent } from './income-expense/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SitebarComponent } from './shared/sitebar/sitebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IncomeExpenseComponent,
+    ChartComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SitebarComponent
   ],
   imports: [
     BrowserModule
